c7ab6f679af12e94f5ab021c7060d9c26334eae6
Corte01 <giacomo.cortese@stud.tecnicosuperiorekennedy.it>
2021-03-03 10:00:48 +0100

    secondo commit

---
+++ ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -0,0 +1,12 @@
+package com.itskennedy.corsoCloudDeveloper;
+
+public class ClasseProva {
+ 
+	public static void main(String[] args) {
+		System.out.println("Prova");
+	}
+	
+	private void caso() {
+		System.out.println("ok");
+	}
+}
