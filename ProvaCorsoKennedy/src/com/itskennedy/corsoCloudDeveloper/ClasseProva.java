349437b6f54b26136d6997f553badcb4d1c4ec2c
Corte01 <giacomo.cortese@stud.tecnicosuperiorekennedy.it>
2021-03-03 10:12:28 +0100

    ultima modifica

--- ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
+++ ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -3,9 +3,7 @@ package com.itskennedy.corsoCloudDeveloper;
 public class ClasseProva {
  
 	public static void main(String[] args) {
-		System.out.println("Prova");
-		System.out.println("ciao cambio");
-		System.out.println("gywfdgkhs");
+		String s="ciao";
 	}
 	
 	private void caso() {
